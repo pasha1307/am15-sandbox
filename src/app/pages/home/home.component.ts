5b1336cd9dd658e8f48dec6ff08b554ff38edd7a
Krotov <Pavel.Krotov@gdit.com>
2023-03-23 07:14:50 -0400

    updated mock up object is added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {APP_DROPDOWNS_OBJ} from "../../../assets/mock-data/dropdown-values-data";
 import {DropdownsService} from "../../services/dropdowns.service";
 import {AppealService} from "../../services/appeal.service";
+import {APL_ADJUD_SAMPLE} from "../../../assets/mock-data/adjudication-object-data";
+import {APL_INTAKE_ARR} from "../../../assets/mock-data/intake-object-data";
 
 @Component({
     selector: 'app-home',
@@ -9,6 +11,8 @@ import {AppealService} from "../../services/appeal.service";
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    adjObject =APL_ADJUD_SAMPLE;
+    intakeObjectArr =APL_INTAKE_ARR;
     allDrops = APP_DROPDOWNS_OBJ;
     allTypes = this.allDrops.result.map(el => {
         return {
@@ -24,6 +28,8 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
+        console.log('ADJ OBJ', this.adjObject.adjudication);
+        console.log('INTAKE OBJ', this.intakeObjectArr[0]);
       this.aplService.getArr().subscribe(r => {
           console.log('Home Arr', r);
       })
